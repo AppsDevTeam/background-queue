63739fc34a3ad01a4f0ea24ceed97b49d7bc80dd
Jan Nováček <honza@appsdevteam.com>
2020-06-11 09:17:35 +0200

    Možnost předat commandu adt:backgroundQueue:process parametr ID, který spustí command pro jeden konkrétní záznam (ve stavu 1, 4, 5 nebo 6).

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -9,12 +9,15 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class ProcessCommand extends Command {
 
+	const PARAM_ID = "id";
+
 	/** @var \ADT\BackgroundQueue\Queue */
 	protected $queue;
 
 	protected function configure() {
 		$this->setName('adt:backgroundQueue:process');
 		$this->setDescription('Zavolá callback pro všechny nezpracované záznamy z DB a zpracuje jak kdyby to zpracoval consumer.');
+		$this->addArgument(self::PARAM_ID, InputArgument::OPTIONAL, "Param 1.");
 	}
 
 	/**
@@ -25,8 +28,13 @@ class ProcessCommand extends Command {
 		$this->queue = $this->getHelper('container')->getByType(\ADT\BackgroundQueue\Queue::class);
 	}
 
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @throws \Exception
+	 */
 	protected function execute(InputInterface $input, OutputInterface $output) {
-		$this->queue->processUnprocessedEntities();
+		$this->queue->processUnprocessedEntities( $input->getArgument(self::PARAM_ID));
 	}
 
 }

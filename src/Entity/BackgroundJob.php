55cdb97e292c1e5791d31ac3819fd4c432abdf3e
Tomáš Kudělka <thorewi@gmail.com>
2023-01-16 19:53:05 +0100

    Update BackgroundJob.php

--- src/Entity/BackgroundJob.php
+++ src/Entity/BackgroundJob.php
@@ -1,324 +1,269 @@
 <?php
 
-namespace ADT\BackgroundQueue;
+namespace ADT\BackgroundQueue\Entity;
 
-use ADT\BackgroundQueue\Entity\BackgroundJob;
-use ADT\BackgroundQueue\Exception\TemporaryErrorException;
-use ADT\BackgroundQueue\Exception\WaitingException;
 use DateTimeImmutable;
-use Doctrine\DBAL\Connection;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\NonUniqueResultException;
-use Doctrine\ORM\NoResultException;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
-use Doctrine\ORM\QueryBuilder;
-use Exception;
-use Throwable;
-use Tracy\Debugger;
-use Tracy\ILogger;
+use Doctrine\ORM\Mapping as ORM;
 
-class BackgroundQueue
+/**
+ * @ORM\Entity
+ * @ORM\Table(indexes={@ORM\Index(name="IDX_BACKGROUNDJOB_IDENTIFIER", columns={"identifier"}), @ORM\Index(name="IDX_BACKGROUNDJOB_STATE", columns={"state"})})
+ */
+#[ORM\Index(name: 'IDX_BACKGROUNDJOB_IDENTIFIER', columns: ['identifier'])]
+#[ORM\Index(name: 'IDX_BACKGROUNDJOB_STATE', columns: ['state'])]
+#[ORM\Entity]
+final class BackgroundJob
 {
-	private array $config;
+	const STATE_READY = 1; // připraveno
+	const STATE_PROCESSING = 2; // zpracovává se
+	const STATE_FINISHED = 3; // dokončeno
+	const STATE_TEMPORARILY_FAILED = 4; // opakovatelná chyba (např. nedostupné API)
+	const STATE_PERMANENTLY_FAILED = 5; // kritická chyba (např. chyba v implementaci)
+	const STATE_WAITING = 6; // ceka na pristi zpracovani
+	const STATE_REDUNDANT = 7; // je nadbytecny (kdyz isUnique = true)
 
-	private EntityManagerInterface $em;
-
-	/** @var Closure[]  */
-	private array $onShutdown = [];
+	const READY_TO_PROCESS_STATES = [
+		self::STATE_READY => self::STATE_READY,
+		self::STATE_TEMPORARILY_FAILED => self::STATE_TEMPORARILY_FAILED,
+		self::STATE_WAITING => self::STATE_WAITING
+	];
 
 	/**
-	 * @throws ORMException
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue
+	 * @internal
 	 */
-	public function __construct(array $config)
-	{
-		$this->config = $config;
+	#[ORM\Id]
+	#[ORM\Column(type: 'integer')]
+	#[ORM\GeneratedValue]
+	private ?int $id = null;
 
-		/** @var Connection $connection */
-		$connection = $config['doctrineDbalConnection'];
-		$this->em = EntityManager::create($connection->getParams(), $config['doctrineOrmConfiguration']);
+	/**
+	 * @ORM\Column(type="string", nullable=false)
+	 */
+	#[ORM\Column(type: 'string', nullable: false)]
+	private string $queue;
+
+	/**
+	 * @ORM\Column(type="string", length=255, nullable=false)
+	 */
+	#[ORM\Column(type: 'string', length: 255, nullable: false)]
+	private string $callbackName;
+
+	/**
+	 * @ORM\Column(type="blob", nullable=false)
+	 * @var resource
+	 */
+	#[ORM\Column(type: 'blob', nullable: false)]
+	private $parameters;
+
+	/**
+	 * @ORM\Column(type="integer", length=1, nullable=false)
+	 */
+	#[ORM\Column(type: 'integer', length: 1, nullable: false)]
+	private int $state = self::STATE_READY;
+
+	/**
+	 * @ORM\Column(type="datetime_immutable", nullable=false)
+	 */
+	#[ORM\Column(type: 'datetime_immutable', nullable: false)]
+	private DateTimeImmutable $createdAt;
+
+	/**
+	 * @ORM\Column(type="datetime_immutable", nullable=true)
+	 */
+	#[ORM\Column(type: 'datetime_immutable', nullable: true)]
+	private ?DateTimeImmutable $lastAttemptAt = null;
+
+	/**
+	 * @ORM\Column(type="integer", nullable=false, options={"default":0})
+	 */
+	#[ORM\Column(type: 'integer', nullable: false, options: ['default' => 0])]
+	private int $numberOfAttempts = 0;
+
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 */
+	#[ORM\Column(type: 'text', nullable: true)]
+	private ?string $errorMessage = null;
+
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	#[ORM\Column(type: 'string', nullable: true)]
+	private ?string $serialGroup = null;
+
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	#[ORM\Column(type: 'string', nullable: true)]
+	private ?string $identifier = null;
+
+	/**
+	 * @ORM\Column(type="boolean", options={"default":0})
+	 */
+	#[ORM\Column(type: 'boolean', options: ['default' => 0])]
+	private bool $isUnique = false;
+
+	final public function __construct()
+	{
+		$this->createdAt = new DateTimeImmutable();
+
+	}
+
+	final public function __clone()
+	{
+		$this->id = null;
+	}
+
+	/** @noinspection PhpUnused */
+	final public function getId(): ?int
+	{
+		return $this->id;
+	}
+
+	/** @noinspection PhpUnused */
+	public function getQueue(): string
+	{
+		return $this->queue;
+	}
+
+	/** @noinspection PhpUnused */
+	public function setQueue(string $queue): self
+	{
+		$this->queue = $queue;
+		return $this;
+	}
+
+	/** @noinspection PhpUnused */
+	final public function getCallbackName(): string
+	{
+		return $this->callbackName;
+	}
+
+	/** @noinspection PhpUnused */
+	final public function setCallbackName(string $callbackName): self
+	{
+		$this->callbackName = $callbackName;
+		return $this;
+	}
+
+	/** @noinspection PhpUnused */
+	final public function getSerialGroup(): ?string
+	{
+		return $this->serialGroup;
+	}
+
+	/** @noinspection PhpUnused */
+	final public function setSerialGroup(?string $serialGroup): self
+	{
+		$this->serialGroup = $serialGroup;
+		return $this;
 	}
 
 	/**
+	 * @noinspection PhpUnused
+	 * @return array
+	 */
+	final public function getParameters(): array
+	{
+		return unserialize(stream_get_contents($this->parameters));
+	}
+
+	/**
+	 * @noinspection PhpUnused
 	 * @param object|array|string|int|float|bool|null $parameters
-	 * @throws Exception
-	 * @Suppress("unused")
 	 */
-	public function publish(
-		string $callbackName,
-		$parameters = null,
-		?string $serialGroup = null,
-		?string $identifier = null,
-		bool $isUnique = false
-	): void
+	final public function setParameters($parameters): self
 	{
-		if (!$callbackName) {
-			throw new Exception('The job does not have the required parameter "callbackName" set.');
-		}
-
-		if (!isset($this->config['callbacks'][$callbackName])) {
-			throw new Exception('Callback "' . $callbackName . '" does not exist.');
-		}
-
-		if ($isUnique && !$serialGroup) {
-			throw new Exception('Parameter "serialGroup has to be set if "isUnique" is true.');
-		}
-
-		$entity = new BackgroundJob();
-		$entity->setQueue($this->config['queue']);
-		$entity->setCallbackName($callbackName);
-		$entity->setParameters($parameters);
-		$entity->setSerialGroup($serialGroup);
-		$entity->setIdentifier($identifier);
-		$entity->setIsUnique($isUnique);
-
-		$this->onShutdown[] = function () use ($entity) {
-			$this->save($entity);
-
-			if ($this->config['amqpPublishCallback']) {
-				$this->doPublish($entity);
-			}
-		};
-	}
-	
-	/** @internal */
-	public function doPublish(BackgroundJob $entity)
-	{
-		try {
-			$this->config['amqpPublishCallback']([$entity->getId()]);
-		} catch (Exception $e) {
-			self::logException('Unexpected error occurred.', $entity, $e);
-
-			$entity->setState(BackgroundJob::STATE_TEMPORARILY_FAILED)
-				->setErrorMessage($e->getMessage());
-			$this->save($entity);
-		}
+		$this->parameters = serialize(is_array($parameters) ? $parameters : [$parameters]);
+		return $this;
 	}
 
-	/**
-	 * @param int|BackgroundJob $entity
-	 * @return void
-	 * @throws Exception
-	 */
-	public function process($entity): void
+	/** @noinspection PhpUnused */
+	final public function getState(): int
 	{
-		if (is_int($entity)) {
-			$id = $entity;
-
-			/** @var BackgroundJob $entity */
-			$entity = $this->getRepository()->find($id);
-
-			// zalogovat (a smazat z RabbitMQ DB)
-			if (!$entity) {
-				self::logException('No job found for ID "' . $id . '"');
-				return;
-			}
-		}
-
-		// Zpráva není ke zpracování v případě, že nemá stav READY nebo ERROR_REPEATABLE
-		// Pokud při zpracování zprávy nastane chyba, zpráva zůstane ve stavu PROCESSING a consumer se ukončí.
-		// Další consumer dostane tuto zprávu znovu, zjistí, že není ve stavu pro zpracování a ukončí zpracování (return).
-		// Consumer nespadne (zpráva se nezačne zpracovávat), metoda process() vrátí TRUE, zpráva se v RabbitMq se označí jako zpracovaná.
-		if (!$entity->isReadyForProcess()) {
-			self::logException('Unexpected state', $entity);
-			return;
-		}
-
-		if ($this->isRedundant($entity)) {
-			$entity->setState(BackgroundJob::STATE_REDUNDANT);
-			$this->save($entity);
-			return;
-		}
-
-		if ($previousEntity = $this->getPreviousUnfinishedJob($entity)) {
-			try {
-				$entity->setState(BackgroundJob::STATE_TEMPORARILY_FAILED);
-				$entity->setErrorMessage('Waiting for job ID ' . $previousEntity->getId());
-				$this->save($entity);
-			} catch (Exception $e) {
-				self::logException('Unexpected error occurred.', $entity, $e);
-			}
-			return;
-		}
-
-		if (!isset($this->config['callbacks'][$entity->getCallbackName()])) {
-			self::logException('Callback "' . $entity->getCallbackName() . '" does not exist.', $entity);
-			return;
-		}
-
-		$callback = $this->config['callbacks'][$entity->getCallbackName()];
-
-		// změna stavu na zpracovává se
-		try {
-			$entity->setState(BackgroundJob::STATE_PROCESSING);
-			$entity->setLastAttemptAt(new DateTimeImmutable());
-			$entity->increaseNumberOfAttempts();
-			$this->save($entity);
-		} catch (Exception $e) {
-			self::logException('Unexpected error occurred', $entity, $e);
-			return;
-		}
-
-		// zpracování callbacku
-		// pokud metoda vyhodí TemporaryErrorException, job nebyl zpracován a zpracuje se příště
-		// pokud se vyhodí jakákoliv jiný error nebo exception implementující Throwable, job nebyl zpracován a jeho zpracování se již nebude opakovat
-		// v ostsatních případech vše proběhlo v pořádku, nastaví se stav dokončeno
-		$e = null;
-		try {
-			// $callback is actually a Statement, that's why we must pass $entity->getParameters() as an array and unpack it inside the Statement
-			$callback($entity->getParameters());
-			$state = BackgroundJob::STATE_FINISHED;
-		} catch (Throwable $e) {
-			switch (get_class($e)) {
-				case TemporaryErrorException::class: $state = BackgroundJob::STATE_TEMPORARILY_FAILED; break;
-				case WaitingException::class: $state = BackgroundJob::STATE_WAITING; break;
-				default: $state = BackgroundJob::STATE_PERMANENTLY_FAILED;
-			}
-		}
-
-		// zpracování výsledku
-		try {
-			$entity->setState($state)
-				->setErrorMessage($e ? $e->getMessage() : null);
-			$this->save($entity);
-
-			if ($state === BackgroundJob::STATE_PERMANENTLY_FAILED) {
-				// odeslání emailu o chybě v callbacku
-				self::logException('Permanent error occured', $entity, $e);
-			} elseif ($state === BackgroundJob::STATE_TEMPORARILY_FAILED) {
-				// pri urcitem mnozstvi neuspesnych pokusu posilat email
-				if ($this->config['notifyOnNumberOfAttempts'] && $this->config['notifyOnNumberOfAttempts'] === $entity->getNumberOfAttempts()) {
-					self::logException('Number of attempts reached ' . $entity->getNumberOfAttempts(), $entity, $e);
-				}
-			}
-		} catch (Exception $innerEx) {
-			self::logException('Unexpected error occurred', $entity, $innerEx);
-		}
+		return $this->state;
 	}
 
-	public function getUnfinishedJobIdentifiers(array $identifiers = []): array
+	/** @noinspection PhpUnused */
+	final public function setState(int $state): self
 	{
-		$qb = $this->createQueryBuilder();
-
-		$qb->andWhere('e.state != :state')
-			->setParameter('state', BackgroundJob::STATE_FINISHED);
-
-		if ($identifiers) {
-			$qb->andWhere('e.identifier IN (:identifier)')
-				->setParameter('identifier', $identifiers);
-		} else {
-			$qb->andWhere('e.identifier IS NOT NULL');
-		}
-
-		$qb->select('e.identifier')->groupBy('e.identifier');
-
-		$unfinishedJobIdentifiers = [];
-		foreach ($qb->getQuery()->getResult() as $_entity) {
-			$unfinishedJobIdentifiers[] = $_entity['identifier'];
-		}
-
-		return $unfinishedJobIdentifiers;
+		$this->state = $state;
+		return $this;
 	}
 
-	public function getConfig(): array
+	/** @noinspection PhpUnused */
+	final public function getCreatedAt(): DateTimeImmutable
 	{
-		return $this->config;
+		return $this->createdAt;
 	}
 
-	/**
-	 * @internal
-	 */
-	public function createQueryBuilder(): QueryBuilder
+	/** @noinspection PhpUnused */
+	final public function getLastAttemptAt(): ?DateTimeImmutable
 	{
-		return $this->getRepository()->createQueryBuilder('e')
-			->andWhere('e.queue = :queue')
-			->setParameter('queue', $this->config['queue']);
+		return $this->lastAttemptAt;
 	}
 
-	/**
-	 * @internal
-	 * @Suppress("unused")
-	 */
-	public function onShutdown(): void
+	/** @noinspection PhpUnused */
+	final public function setLastAttemptAt(DateTimeImmutable $lastAttemptAt): self
 	{
-		foreach ($this->onShutdown as $_handler) {
-			$_handler->call($this);
-		}
+		$this->lastAttemptAt = $lastAttemptAt;
+		return $this;
 	}
 
-	/**
-	 * @throws NonUniqueResultException
-	 * @throws NoResultException
-	 */
-	private function isRedundant(BackgroundJob $entity): bool
+	/** @noinspection PhpUnused */
+	final public function getNumberOfAttempts(): int
 	{
-		if (!$entity->isUnique()) {
-			return false;
-		}
-
-		$qb = $this->createQueryBuilder()
-			->select('COUNT(e)');
-
-		$qb->andWhere('e.identifier = :identifier')
-			->setParameter('identifier', $entity->getIdentifier());
-
-		$qb->andWhere('e.id < :id')
-			->setParameter('id', $entity->getId());
-
-		return (bool) $qb->getQuery()->getSingleScalarResult();
+		return $this->numberOfAttempts;
 	}
 
-	/**
-	 * @throws NonUniqueResultException
-	 */
-	private function getPreviousUnfinishedJob(BackgroundJob $entity): ?BackgroundJob
+	/** @noinspection PhpUnused */
+	final public function increaseNumberOfAttempts(): self
 	{
-		if (!$entity->getSerialGroup()) {
-			return null;
-		}
-
-		$qb = $this->createQueryBuilder();
-
-		$qb->andWhere('e.state != :state')
-			->setParameter('state', BackgroundJob::STATE_FINISHED);
-
-		$qb->andWhere('e.serialGroup = :serialGroup')
-			->setParameter('serialGroup', $entity->getSerialGroup());
-
-		if ($entity->getId()) {
-			$qb->andWhere('e.id < :id')
-				->setParameter('id', $entity->getId());
-		}
-
-		$qb->orderBy('e.id');
-
-		return $qb->getQuery()
-			->setMaxResults(1)
-			->getOneOrNullResult();
+		$this->numberOfAttempts++;
+		return $this;
 	}
 
-	private function getRepository(): EntityRepository
+	/** @noinspection PhpUnused */
+	final public function getErrorMessage(): ?string
 	{
-		return $this->em->getRepository(BackgroundJob::class);
+		return $this->errorMessage;
 	}
 
-	/**
-	 * @throws OptimisticLockException
-	 * @throws ORMException
-	 */
-	private function save(BackgroundJob $entity): void
+	/** @noinspection PhpUnused */
+	final public function setErrorMessage(?string $errorMessage): self
 	{
-		if (!$entity->getId()) {
-			$this->em->persist($entity);
-		}
-
-		$this->em->flush();
+		$this->errorMessage = $errorMessage;
+		return $this;
 	}
 
-	private static function logException(string $errorMessage, ?BackgroundJob $entity = null, ?Throwable $e = null): void
+	public function getIdentifier(): ?string
 	{
-		Debugger::log(new Exception('BackgroundQueue: ' . $errorMessage  . ($entity ? ' (ID: ' . $entity->getId() . '; State: ' . $entity->getState() . ')' : ''), 0, $e), ILogger::CRITICAL);
+		return $this->identifier;
+	}
+
+	public function setIdentifier(?string $identifier): self
+	{
+		$this->identifier = $identifier;
+		return $this;
+	}
+
+	public function isUnique(): bool
+	{
+		return $this->isUnique;
+	}
+
+	public function setIsUnique(bool $isUnique): self
+	{
+		$this->isUnique = $isUnique;
+		return $this;
+	}
+
+	/** @noinspection PhpUnused */
+	final public function isReadyForProcess(): bool
+	{
+		return isset(self::READY_TO_PROCESS_STATES[$this->state]);
 	}
 }

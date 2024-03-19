a467a43c63498ae9b712a1774a7baa035eb9fad9
Viktor Masicek <viktor@appsdevteam.com>
2024-03-19 14:12:09 +0100

    Enable set number of jobs consumed by one consumer in one process

--- src/Console/ConsumeCommand.php
+++ src/Console/ConsumeCommand.php
@@ -5,6 +5,7 @@ namespace ADT\BackgroundQueue\Console;
 use ADT\BackgroundQueue\Broker\Consumer;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsumeCommand extends \Symfony\Component\Console\Command\Command
@@ -22,12 +23,21 @@ class ConsumeCommand extends \Symfony\Component\Console\Command\Command
 	{
 		$this->setName('background-queue:consume');
 		$this->addArgument('queue', InputArgument::REQUIRED);
+		$this->addOption('jobs', 'j', InputOption::VALUE_REQUIRED, 'Number of jobs consumed by one consumer in one process', 1);
 		$this->setDescription('Start consumer.');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$this->consumer->consume($input->getArgument('queue'));
+		$jobs = $input->getOption('jobs');
+		if (!is_numeric($jobs)) {
+			$output->writeln("<error>Option --jobs has to be integer</error>");
+			return 1;
+		}
+
+		for ($i = 0; $i < (int)$jobs; $i++) {
+			$this->consumer->consume($input->getArgument('queue'));
+		}
 
 		return 0;
 	}

d0213bd9c21a88cb0665446c0ce861fa22a44964
Viktor Masicek <viktor@appsdevteam.com>
2024-06-03 14:13:04 +0200

    Oprava priorit
    - Zrušení detekce počtu zpráv v RabbitMQ, protože detekce nefungovala jak bychom potřebali.
    - Vždy se připojíme na všechny fronty.
    - Při zpracování zprávy se nejprve odpojíme od všech front a od knanálu, aby byly vyhrazené zprávy uvolněny pro ostatní oknzumery
    - typo

--- src/Broker/PhpAmqpLib/Consumer.php
+++ src/Broker/PhpAmqpLib/Consumer.php
@@ -26,44 +26,39 @@ class Consumer implements \ADT\BackgroundQueue\Broker\Consumer
 		// TODO Do budoucna cheme podporovat libovolné priority a ne pouze jejich výčet.
 		//      Zde si musíme vytáhnout seznam existujících front. To lze přes HTTP API pomocí CURL.
 
-		// Nejprve se chceme kouknout, jestli není zaslána zpráva k ukončení, proto na první místo dáme supereme frontu.
+		// Nejprve se chceme kouknout, jestli není zaslána zpráva k ukončení, proto na první místo dáme TOP_PRIORITY frontu.
 		array_unshift($priorities, Manager::QUEUE_TOP_PRIORITY);
 
+		// Sestavíme si seznam názvů front v RabbitMQ (tedy včetně priorit) a všechny inicializujeme
 		$queuesWithPriorities = [];
 		foreach ($priorities as $priority) {
-			$queuesWithPriorities[] = $this->manager->getQueueWithPriority($queue, $priority);;
-		}
-
-		// Pokud jsou všechny prázdné, nabinduju se na všechny dle priority.
-		// Jinak se nabinduju jen na první neprázdnou, abych z ní odbavil zprávu.
-		$queuesToBind = $queuesWithPriorities;
-		foreach ($queuesWithPriorities as $queueWithPriority) {
+			$queueWithPriority = $this->manager->getQueueWithPriority($queue, $priority);
+			$queuesWithPriorities[] = $queueWithPriority;
 			$this->manager->createQueue($queueWithPriority);
-			if ($this->manager->getQueueMessagesCount($queueWithPriority)) {
-				$queuesToBind = [$queueWithPriority];
-				break;
-			}
 		}
 
 		$this->manager->setupQos();
 
-		foreach ($queuesToBind as $queue) {
+		foreach ($queuesWithPriorities as $queue) {
+			$this->manager->getChannel()->basic_consume($queue, $queue, false, false, false, false, function(AMQPMessage $msg) use ($queuesWithPriorities) {
 
-			$this->manager->getChannel()->basic_consume($queue, $queue, false, false, false, false, function(AMQPMessage $msg) use ($queuesToBind) {
 				$msg->ack();
+
+				// Odpojím se od všech nabindovaných front
+				foreach ($queuesWithPriorities as $queuesWithPriority) {
+					$msg->getChannel()->basic_cancel($queuesWithPriority);
+				}
+
+				// Odpojím se od kanálu, abych uvolnil zprávy vyhrazené pro ostatní nabindované callbacky na ostatní fronty a zprávy mohly okamžitě zpracovat jiní konzumeři
+				$this->manager->closeChannel();
 
 				if ($msg->getBody() === Producer::DIE) {
 					die();
 				}
 
-				$queue = $msg->getConsumerTag();
-				list($queueWithoutPriority, $priority) = $this->manager->parseQueueAndPriority($queue);
-				$this->backgroundQueue->process((int)$msg->getBody(), $queueWithoutPriority, $priority);
-
-				// Odpojím se od všech nabindovaných front
-				foreach ($queuesToBind as $queue) {
-					$msg->getChannel()->basic_cancel($queue);
-				}
+				$queuesWithPriority = $msg->getConsumerTag();
+				list($queue, $priority) = $this->manager->parseQueueAndPriority($queuesWithPriority);
+				$this->backgroundQueue->process((int)$msg->getBody(), $queue, $priority);
 			});
 		}
 

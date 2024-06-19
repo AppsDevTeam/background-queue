469dbbbdd2354a5441ad8fd17667612fdd2aa5f2
Viktor Masicek <viktor@appsdevteam.com>
2024-06-19 13:36:49 +0200

    Oprava odhlašování od connection a channel
    - Při odhlašování od channel vždy čekáme na "wait_for_pending_acks_returns"
    - Pro odhlašování od channel použijeme jednotnou metodu
    - Callback pro shutdown registrujeme pouze jednou

--- src/Broker/PhpAmqpLib/Manager.php
+++ src/Broker/PhpAmqpLib/Manager.php
@@ -16,6 +16,7 @@ class Manager
 
 	private ?AMQPStreamConnection $connection = null;
 	private ?AMQPChannel $channel = null;
+	private bool $shutdownRegistered = false;
 
 	private array $initQueues;
 	private array $initExchanges;
@@ -36,6 +37,14 @@ class Manager
 		return $this->connection;
 	}
 
+	public function closeConnection(): void
+	{
+		if ($this->connection) {
+			$this->connection->close();
+			$this->connection = null;
+		}
+	}
+
 	public function getChannel(): AMQPChannel
 	{
 		if (!$this->channel) {
@@ -49,11 +58,14 @@ class Manager
 					throw new Exception("Code: $replyCode, Text: $replyText, Exchange: $exchange, Routing Key: $routingKey");
 				}
 			);
-			register_shutdown_function(function() {
-				$this->channel->wait_for_pending_acks_returns();
-				$this->channel->close();
-				$this->connection->close();
-			});
+
+			if (!$this->shutdownRegistered) {
+				register_shutdown_function(function() {
+					$this->closeChannel();
+					$this->closeConnection();
+				});
+				$this->shutdownRegistered = true;
+			}
 		}
 
 		return $this->channel;
@@ -61,11 +73,11 @@ class Manager
 
 	public function closeChannel(): void
 	{
-		$channel = $this->channel;
-		$this->channel = null;
-		$this->initQos = false;
-		if ($channel) {
-			$channel->close();
+		if ($this->channel) {
+			$this->channel->wait_for_pending_acks_returns();
+			$this->channel->close();
+			$this->channel = null;
+			$this->initQos = false;
 		}
 	}
 

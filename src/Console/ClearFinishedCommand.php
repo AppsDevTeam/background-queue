5a2c176e674ae928c2a0528628e464d881777071
Tomáš Kudělka <thorewi@gmail.com>
2025-01-30 06:51:47 +0100

    Update ClearFinishedCommand.php

--- src/Console/ClearFinishedCommand.php
+++ src/Console/ClearFinishedCommand.php
@@ -55,7 +55,7 @@ class ClearFinishedCommand extends Command
 				->setParameter('ago', (new DateTime('midnight'))->modify('-' . $input->getArgument("days") . ' days')->format('Y-m-d H:i:s'));
 		}
 
-		$qb->execute();
+		$qb->executeStatement();
 
 		return 0;
 	}
